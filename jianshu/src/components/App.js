5a2fccb1fd001f81122d9731c02c627a1eae9052
a77321a <1817169988@qq.com>
2019-08-15 11:31:08 +0800

    pureComponent

--- jianshu/src/components/App.js
+++ jianshu/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Button, Input } from 'element-react';
-
+// pureComponent
 class App extends Component {
   constructor(props) {
     super(props)
