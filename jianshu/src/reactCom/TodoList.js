1fdd3ea1370feb090c6aced502c4ab8a5ac1d177
a77321a <1817169988@qq.com>
2019-10-30 18:03:17 +0800

    redux-react

--- jianshu/src/reactCom/TodoList.js
+++ jianshu/src/reactCom/TodoList.js
@@ -3,16 +3,21 @@
  * @Author: 辛顺宁
  * @Date: 2019-10-29 17:44:05
  * @LastEditors: 辛顺宁
- * @LastEditTime: 2019-10-29 17:45:50
+ * @LastEditTime: 2019-10-30 17:53:16
  */
 import React, { Component } from 'react'
+import store from '../reactStore';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = store.getState()
+  }
   render () {
     return (
       <div>
         <div>
-          <input type="text" />
+          <input type="text" value={this.state.inputValue} />
           <input type="button" value="提交" />
         </div>
         <ul>
--- jianshu/src/reactStore/index.js
+++ jianshu/src/reactStore/index.js
@@ -3,10 +3,11 @@
  * @Author: 辛顺宁
  * @Date: 2019-10-29 17:46:19
  * @LastEditors: 辛顺宁
- * @LastEditTime: 2019-10-29 17:46:58
+ * @LastEditTime: 2019-10-30 17:51:45
  */
 import { createStore } from 'redux'
+import reducer from './reducer';
 
-const store = createStore();
+const store = createStore(reducer);
 
 export default store
